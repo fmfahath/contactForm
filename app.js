5cb86a3b47b0910c5442d32e9979b77c92971346
fmfahath <fmfahath.dev@gmail.com>
2023-12-21 14:41:57 +0530

    name validation function added

--- app.js
+++ app.js
@@ -4,8 +4,44 @@ const emailEl = document.getElementById('email');
 const phoneNoEl = document.getElementById('phoneNo');
 const msgEl = document.getElementById('message');
 
+const  nameErrorEl = document.getElementById('name-error');
+
+//form validation---------------------------
+
+function validateName(){
+    let name = document.getElementById('name').value;
+    
+    if(name.length == 0){
+        nameErrorEl.innerHTML = '*Name required';
+        return false;
+    }
+    // else if(!name.match(/^[A-Za-a]*\s{1}[A-Za-z]*$/)){
+    //     console.log("full name required",name);
+    //     nameErrorEl.innerHTML = 'Full name required';
+    //     return false;
+    // }
+    else{
+        nameErrorEl.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+        return true;
+    }
+}
 
 
+let text = " hello";
+let result = text.trim();
+console.log(text, text.length);
+console.log(result, result.length);
+
+
+
+
+
+
+
+
+
+
+// submir form data to gmail--------------------- 
 function sendEmail(){
     let bodyMessage = 
         `Name:${nameEl.value}<br> 
@@ -34,5 +70,5 @@ function sendEmail(){
 formEl.addEventListener('submit', (e) => {
     console.log('clicke');
     e.preventDefault();
-    sendEmail();
+    // sendEmail();
 });
